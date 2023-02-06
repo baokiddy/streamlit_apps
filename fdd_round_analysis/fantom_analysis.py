994bffb7236646f0f3e7f6f0f6d953e9b1d6ec63
akathm <101077243+akathm@users.noreply.github.com>
2023-02-05 16:58:47 -0800

    Update fantom_analysis.py
    troubleshooting googlesheet input.

--- fdd_round_analysis/fantom_analysis.py
+++ fdd_round_analysis/fantom_analysis.py
@@ -5,22 +5,22 @@ import json
 import numpy as np
 from gsheetsdb import connect
 
-siteHeader = st.container()
-dataExploration = st.container()
-newFeatures = st.container()
-modelTraining = st.container()
+dataset_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTqCQ3-PFdTVUqEr2bGO4fO88w6zgXdwKLDPz6tarQyDscv6e6FekNiY4uZimyMDxXeocXQ2yZ8nIFr/pub?gid=451420609&single=true&output=csv"
 
-rc_dataExploration, ls_dataExploration, sf_dataExploration, fn_dataExploration = st.tabs(["Revoke.cash", "LoanShark", "SymphonyFinance", "FantomNames"])
+# read csv from a URL
+@st.experimental_memo
+def get_data() -> pd.DataFrame:
+    return pd.read_csv(dataset_url)
 
-with siteHeader:
-  st.title('Welcome to Fantom Round Analysis!')
-  st.text('In this project we are going to breakdown analysis of the round contributions and identify possible sybil behaviour')
+df = get_data()
 
-  df = pd.DataFrame({"one": ['token'], "two": ['id'], "three": ['amount']})
-  st.write(df)
-  
-  gsheet_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTqCQ3-PFdTVUqEr2bGO4fO88w6zgXdwKLDPz6tarQyDscv6e6FekNiY4uZimyMDxXeocXQ2yZ8nIFr/pub?gid=451420609&single=true&output=csv"
-  conn = connect()
-  rows = conn.execute(f'SELECT * FROM "{gsheet_url}"')
-  df_gsheet = pd.DataFrame(rows)
-  st.write(df_gsheet)
+st.set_page_config(
+    page_title="Fantom Alpha Grants Round Dashboard",
+    page_icon="📊",
+    layout="wide",
+)
+
+st.title("Round-Monitoring Dashboard")
+
+job_filter = st.selectbox("Select the Project", pd.unique(df["destination_wallet"]))
+df = df[df["destiantion_wallet"] == job_filter]
