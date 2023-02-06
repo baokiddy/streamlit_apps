9a4da6695fe95c7f8eef220446d8e24973e2e567
akathm <101077243+akathm@users.noreply.github.com>
2023-02-05 16:36:18 -0800

    Update fantom_analysis.py

--- fdd_round_analysis/fantom_analysis.py
+++ fdd_round_analysis/fantom_analysis.py
@@ -17,7 +17,7 @@ with siteHeader:
   st.title('Welcome to Fantom Round Analysis!')
   st.text('In this project we are going to breakdown analysis of the round contributions and identify possible sybil behaviour')
 
-  gsheet_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTqCQ3-PFdTVUqEr2bGO4fO88w6zgXdwKLDPz6tarQyDscv6e6FekNiY4uZimyMDxXeocXQ2yZ8nIFr/edit?usp=sharing"
+  gsheet_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTqCQ3-PFdTVUqEr2bGO4fO88w6zgXdwKLDPz6tarQyDscv6e6FekNiY4uZimyMDxXeocXQ2yZ8nIFr/pub?gid=451420609&single=true&output=csv"
   conn = connect()
   rows = conn.execute(f'SELECT * FROM "{gsheet_url}"')
   df_gsheet = pd.DataFrame(rows)
