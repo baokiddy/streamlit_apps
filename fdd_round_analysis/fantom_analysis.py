b203fb1b9d8fcec70973a8ceeb8cd0bdb44c7ae5
akathm <101077243+akathm@users.noreply.github.com>
2023-02-05 18:48:15 -0800

    Update fantom_analysis.py

--- fdd_round_analysis/fantom_analysis.py
+++ fdd_round_analysis/fantom_analysis.py
@@ -4,10 +4,6 @@ from pandas.io.json import json_normalize
 import json
 import numpy as np
 
-siteHeader = st.container()
-dataExploration = st.container()
-newFeatures = st.container()
-modelTraining = st.container()
 
 #### ATTEMPT TO CACHE: not operable
 #### URLError: <urlopen error [SSL: CERTIFICATE_VERIFY_FAILED] 
@@ -17,106 +13,86 @@ modelTraining = st.container()
 #    return pd.read_csv(dataset_url)
 #df = get_data()
 
-rc_dataExploration, ls_dataExploration, sf_dataExploration, fn_dataExploration = st.tabs(["Revoke.cash", "LoanShark", "SymphonyFinance", "FantomNames"])
+st.set_page_config(
+    page_title="Fantom Round Dashboard",
+    page_icon="📊",
+    layout="wide",
+)
 
-with siteHeader:
-  st.title('Welcome to Fantom Round Analysis!')
-  st.text('In this project we are going to breakdown analysis of the round contributions and identify possible sybil behaviour')
+fantom_data = pd.read_csv('data/fantom_data_analysis_by_data_subset.csv') 
 
-  ##col1, col2, col3, col4 = st.columns([5, 5, 20, 5])
-  ##with col2:
-  ##  st.image("fantom.png", width=100)
-  ##with col3:
-  ##  st.title("Rounds Dashboard Demo")
-  ##with col4:
-  ##  st.image("gtc.png", width=100)
-    
-  col1, col2, col3 = st.columns(3)
-  col1.metric("Grant applications", "10")
-  col2.metric("Unique contributors", "900")
-  col3.metric("Total amount of contributions", "100000", "-10 from yesterday")
+# read csv from a URL
+@st.experimental_memo
+def get_data() -> pd.DataFrame:
+    return pd.read_csv(fantom_data)
 
-  fantom_data = pd.read_csv('data/fantom_data_analysis_by_data_subset.csv') 
-  fantom_data['amount_donated'] = fantom_data['amount_donated'].astype('string')
+df = get_data()
 
-  st.header('Distribution of donations')
-  st.text('This data is ...')
+st.title("Real-Time / Live Data Science Dashboard")
 
-  ftm_all_projects =  fantom_data[fantom_data['data_subset'] == 'ftm_all_projects']
+# top-level filters
+job_filter = st.selectbox("Select the Project", pd.unique(df["destination_wallet"]))
 
-  ftm_all_distribution = ftm_all_projects[['amount_donated', 'unique_donations']]
-  st.bar_chart(ftm_all_distribution, x='amount_donated', y='unique_donations') 
+# creating a single-element container
+placeholder = st.empty()
 
-  st.header('Sum of donations by categories')
-  st.text('This data is ...')
+# dataframe filter
+df = df[df["job"] == job_filter]
 
-  ftm_all_sum = ftm_all_projects[['amount_donated', 'total_amount_donated']]
-  st.bar_chart(ftm_all_sum, x='amount_donated', y='total_amount_donated') 
+# near real-time / live feed simulation
+for seconds in range(200):
 
-  st.header('Project specific analysis')
-  st.text('This data is ...')
+    df["age_new"] = df["age"] * np.random.choice(range(1, 5))
+    df["balance_new"] = df["balance"] * np.random.choice(range(1, 5))
 
-with rc_dataExploration:
-  st.header('Distribution of all donations')
-  st.text('This data is ...')
+    # creating KPIs
+    avg_age = np.mean(df["age_new"])
 
-  revoke_contributions =  fantom_data[fantom_data['data_subset'] == 'revoke_cash']
+    count_married = int(
+        df[(df["marital"] == "married")]["marital"].count()
+        + np.random.choice(range(1, 30))
+    )
 
-  revoke_distribution = revoke_contributions[['amount_donated', 'unique_donations']]
-  st.bar_chart(revoke_distribution, x='amount_donated', y='unique_donations') 
+    balance = np.mean(df["balance_new"])
 
-  st.header('Sum of donations by categories')
-  st.text('This data is ...')
+    with placeholder.container():
 
-  revoke_sum = revoke_contributions[['amount_donated', 'total_amount_donated']]
-  st.bar_chart(revoke_sum, x='amount_donated', y='total_amount_donated') 
+        # create three columns
+        kpi1, kpi2, kpi3 = st.columns(3)
 
-with ls_dataExploration:
-  st.header('Distribution of all donations')
-  st.text('This data is ...')
+        # fill in those three columns with respective metrics or KPIs
+        kpi1.metric(
+            label="Age ⏳",
+            value=round(avg_age),
+            delta=round(avg_age) - 10,
+        )
+        
+        kpi2.metric(
+            label="Married Count 💍",
+            value=int(count_married),
+            delta=-10 + count_married,
+        )
+        
+        kpi3.metric(
+            label="A/C Balance ＄",
+            value=f"$ {round(balance,2)} ",
+            delta=-round(balance / count_married) * 100,
+        )
 
-  loan_shark_contributions =  fantom_data[fantom_data['data_subset'] == 'loan_shark']
+        # create two columns for charts
+        fig_col1, fig_col2 = st.columns(2)
+        with fig_col1:
+            st.markdown("### First Chart")
+            fig = px.density_heatmap(
+                data_frame=df, y="age_new", x="marital"
+            )
+            st.write(fig)
+            
+        with fig_col2:
+            st.markdown("### Second Chart")
+            fig2 = px.histogram(data_frame=df, x="age_new")
+            st.write(fig2)
 
-  loan_shark_distribution = loan_shark_contributions[['amount_donated', 'unique_donations']]
-  st.bar_chart(loan_shark_distribution, x='amount_donated', y='unique_donations') 
-
-  st.header('Sum of donations by categories')
-  st.text('This data is ...')
-
-  loan_shark_sum = loan_shark_contributions[['amount_donated', 'total_amount_donated']]
-  st.bar_chart(loan_shark_sum, x='amount_donated', y='total_amount_donated') 
-
-
-with sf_dataExploration:
-  st.header('Distribution of all donations')
-  st.text('This data is ...')
-
-  symphony_finance_contributions =  fantom_data[fantom_data['data_subset'] == 'symphony_finance']
-
-  symphony_financce_distribution = symphony_finance_contributions[['amount_donated', 'unique_donations']]
-  st.bar_chart(symphony_financce_distribution, x='amount_donated', y='unique_donations') 
-
-  st.header('Sum of donations by categories')
-  st.text('This data is ...')
-
-  symphony_financce_sum = symphony_finance_contributions[['amount_donated', 'total_amount_donated']]
-  st.bar_chart(symphony_financce_sum, x='amount_donated', y='total_amount_donated') 
-
-
-with fn_dataExploration:
-  st.header('Distribution of all donations')
-  st.text('This data is ...')
-
-  fantom_names_contributions =  fantom_data[fantom_data['data_subset'] == 'fantom_names']
-
-  fantom_names_distribution = fantom_names_contributions[['amount_donated', 'unique_donations']]
-  st.bar_chart(fantom_names_distribution, x='amount_donated', y='unique_donations') 
-
-  st.header('Sum of donations by categories')
-  st.text('This data is ...')
-
-  fantom_names_sum = fantom_names_contributions[['amount_donated', 'total_amount_donated']]
-  st.bar_chart(fantom_names_sum, x='amount_donated', y='total_amount_donated')
-
-  st.markdown("### Detailed Data View")
-  st.dataframe(df)
+        st.markdown("### Detailed Data View")
+        st.dataframe(df)
+        time.sleep(1)
