a0e0c49ecbd5d6984a5f0f6025c0e9b76f80bdb7
baoki <bellaokiddy@gmail.com>
2023-02-10 01:56:30 +0300

    Addition of other Alpha round analysis

--- fdd_round_analysis/pages/Climate Solutions Round.py
+++ fdd_round_analysis/pages/Climate Solutions Round.py
@@ -13,30 +13,111 @@ file_path_votes = 'data/climat_round_votes_raw.json'
 round_name = 'climate'
 
 complete_dataset = data_flattening.processing(file_path_applications, file_path_votes, round_name)
-
+complete_dataset = complete_dataset[['id','token','amount',	'source_wallet', 'project_wallet', 'created_at_x',	'project_id','title', 'project_github',	'project_twitter', 'previous_funding', 'team_size',	'verified_twitter_or_github', 'links_to_github_or_org']]
+main_df = complete_dataset.tail()
 
 with siteHeader:
-  st.title('Climate Solutions Analysis')
-  st.text('In this project we are going to breakdown analysis of the round contributions and identify possible sybil behaviour')
+    st.title('Climate Solutions Analysis')
+    st.text('In this project we are going to breakdown analysis of the round contributions and identify possible sybil behaviour')
 
-  col1, col2, col3 = st.columns(3)
-  col1.metric("Grant applications", "10")
-  col2.metric("Unique contributors", "900")
-  col3.metric("Total amount of contributions", "100000", "-10 from yesterday")
+    votes_dai = votes[votes['token'] == 'DAI']
+    votes_eth = votes[votes['token'] == 'ETH']
 
-  fantom_data = pd.read_csv('data/fantom_data_analysis_by_data_subset.csv') 
-  fantom_data['amount_donated'] = fantom_data['amount_donated'].astype('string')
+    col1, col2, col3, col4 = st.columns(4)
+    col1.metric("Grant applications", f"{apps['project_id'].nunique()}")
+    col2.metric("Unique contributors", f"{votes['source_wallet'].nunique()}")
+    col3.metric("Total contributions (ETH)", str(votes_eth['amount'].sum().round(2)))
+    col4.metric("Total contributions (DAI)", str(votes_dai['amount'].sum().round(2)))
 
-  st.header('Distribution of donations')
-  st.text('This data is ...')
+    
+    gb = GridOptionsBuilder.from_dataframe(main_df)
+   
+    gb.configure_column("amount", type=["numericColumn","numberColumnFilter","customNumericFormat"], precision=2, aggFunc='sum')
+    gb.configure_column("created_at_x", type=["dateColumnFilter","customDateTimeFormat"], custom_format_string='yyyy-MM-dd HH:mm zzz', pivot=True)
 
-  ftm_all_projects =  fantom_data[fantom_data['data_subset'] == 'ftm_all_projects']
+    #configures last row to use custom styles based on cell's value, injecting JsCode on components front end
+    cellsytle_jscode = JsCode("""
+    function(params) {
+        if ((params.value !== undefined) && (params.value !== null)) {
+            return {
+                'color': 'black',
+                'backgroundColor': 'white'
+            }
+        } else {
+            return {
+                'color': 'white',
+                'backgroundColor': 'darkred'
+            }
+        }
+    };
+    """)
 
-  ftm_all_distribution = ftm_all_projects[['amount_donated', 'unique_donations']]
-  st.bar_chart(ftm_all_distribution, x='amount_donated', y='unique_donations') 
+    gb.configure_column("previous_funding", cellStyle=cellsytle_jscode)
 
-  st.header('Sum of donations by categories')
-  st.text('This data is ...')
+    gb.configure_selection('multiple')
+    gb.configure_selection('multiple', use_checkbox=True, groupSelectsChildren=True, groupSelectsFiltered=True)
 
-  ftm_all_sum = ftm_all_projects[['amount_donated', 'total_amount_donated']]
-  st.bar_chart(ftm_all_sum, x='amount_donated', y='total_amount_donated') 
+    gb.configure_grid_options(domLayout='normal')
+    gridOptions = gb.build()
+
+
+    grid_height = 300
+    return_mode_value = 'FILTERED'
+    update_mode_value = 'GRID_CHANGED'
+
+    grid_response = AgGrid(
+        complete_dataset, 
+        gridOptions=gridOptions,
+        height=grid_height, 
+        width='100%',
+        data_return_mode=return_mode_value, 
+        update_mode=update_mode_value,
+        allow_unsafe_jscode=True #Set it to True to allow jsfunction to be injected
+        )
+
+    df = grid_response['data']
+    selected = grid_response['selected_rows']
+    selected_df = pd.DataFrame(selected).apply(pd.to_numeric, errors='coerce')
+
+
+    with st.spinner("Displaying results..."):
+        #displays the chart
+        df['created_at_x'] = pd.to_datetime(df['created_at_x'])
+        chart_data = df.loc[:,['created_at_x','amount']].assign(source='total')
+
+        if not selected_df.empty :
+            selected_data = selected_df.loc[:,['created_at_x','amount']].assign(source='selection')
+            chart_data = pd.concat([chart_data, selected_data])
+
+        # chart_data = pd.melt(chart_data, id_vars=['source'], var_name="date", value_name="quantity")
+        #st.dataframe(chart_data)
+        chart = alt.Chart(data=chart_data).mark_bar().encode(
+            x=alt.X("week(checked_at_x):O"),
+            y=alt.Y("sum(amount):Q"),
+            color=alt.Color('source:N', scale=alt.Scale(domain=['total','selection'])),
+        )
+
+
+        # alt.Chart(df).mark_line().encode(
+        #     x=alt.X('Date', axis=alt.Axis(format='%e %b, %Y')),
+        #     y=alt.Y('Values', scale=alt.Scale(zero=False)),
+        #     color='Key') 
+
+        # chart = alt.Chart(data=chart_data).mark_bar().encode(
+        #     x=alt.X('checked_at_x:N', title=None),
+        #     y=alt.Y('amount:Q', scale=alt.Scale(domain=(0, 12000000))),
+        #     color=alt.Color('source:N', scale=alt.Scale(domain=['total','selection']))
+        # ).properties(
+        #     width=600
+        # ).transform_calculate(
+        #     "source", alt.expr.if_(alt.datum.source == 1, "total", "selection")
+        # ).configure_facet(
+        #     spacing=8
+        # )
+
+        st.header("Amount donated over time")
+        st.markdown("""
+        This chart is built with data returned from the grid. The rows that are selected are identified as shown in the legend.
+        """)
+
+        st.altair_chart(chart, use_container_width=True)
